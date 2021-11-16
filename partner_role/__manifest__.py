8ce25167f963eb5de7b7a070e6ef3df6ef23790b
clementmbr <clement.mombereau@akretion.com.br>
2021-11-16 14:05:39 +0100

    [ADD] partner_role

---
+++ partner_role/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2021 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Role",
+    "description": """
+        Add a "Role" field on Partners and Partners Categories, allowing to relate to
+        the Partner one specific info from one specific Category.
+
+        We separate this model's creation from `dualsun_base` because we need the model
+        to exist before loading "dualsun_base/data/partner_extra_data.xml"
+        in pre_init_hook.
+
+        """,
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "http://akretion.com",
+    "depends": [],
+    "data": ["security/ir.model.access.csv", "views/res_partner_category_views.xml"],
+    "demo": [],
+}
