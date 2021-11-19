1a4e3955675dbcb4d8eaf3578cfbdb44cb18adb5
clementmbr <clement.mombereau@akretion.com.br>
2021-11-19 10:24:56 +0100

    [ADD] l10n_fr_partner_company

---
+++ l10n_fr_partner_company/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "L10n Fr Partner Company",
+    "description": """
+        Move SIRET fields in new res.partner's Company page from module partner_company
+    """,
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "http://akretion.com",
+    "depends": ["l10n_fr_siret", "partner_company"],
+    "data": ["views/res_partner.xml"],
+    "demo": [],
+}
