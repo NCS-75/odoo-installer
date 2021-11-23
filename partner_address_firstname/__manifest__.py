683b5cb298bffef118d653e407f6dfe13fac2bbd
clementmbr <clement.mombereau@akretion.com.br>
2021-11-23 12:25:38 +0100

    [ADD] partner_address_firstname adn partner_address_open_tab

---
+++ partner_address_firstname/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Address Firstname",
+    "description": """
+        Improvements on partner_address used with partner_firstname""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "http://akretion.com",
+    "depends": ["base", "partner_address", "partner_firstname"],
+    "data": ["views/res_partner_views.xml"],
+    "demo": [],
+}
---
+++ partner_address_open_tab/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Address Open Tab",
+    "description": """
+        Improvements on partner_address used with web_widget_open_tab""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "http://akretion.com",
+    "depends": ["base", "partner_address", "web_widget_open_tab"],
+    "data": ["views/res_partner_views.xml"],
+    "demo": [],
+}
