b2feef06f70ddce2c446a7436bcf0b491670e963
clementmbr <clement.mombereau@akretion.com.br>
2021-11-23 13:55:39 +0100

    [ADD] partner_address_account

---
+++ partner_address_account/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Address Account",
+    "description": """
+        Improvements on partner_address used with account module""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "http://akretion.com",
+    "depends": ["base", "partner_address", "account"],
+    "data": ["views/res_partner_views.xml"],
+    "demo": [],
+}
