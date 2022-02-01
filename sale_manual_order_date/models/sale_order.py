cbc4295b1dbcfb033bdfb38d06cb3a2f0ca445ac
clementmbr <clement.mombereau@akretion.com.br>
2022-02-01 15:02:27 -0300

    [ADD] sale_manual_order_date

---
+++ sale_manual_order_date/models/sale_order.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def _prepare_confirmation_values(self):
+        date_order = self.date_order
+        res = super()._prepare_confirmation_values()
+        res["date_order"] = date_order
+        return res
