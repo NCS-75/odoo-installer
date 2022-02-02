0d7155065cbd3044b140a027f9e4e721d1241b74
clementmbr <clement.mombereau@akretion.com.br>
2022-02-02 19:27:19 -0300

    [ADD] crm_aftersale_restrict

---
+++ crm_aftersale_restrict/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Crm Aftersale Restrict",
+    "description": """
+        Restrict security group "Sales/Own Documents Only" to edit Leads only if not in After-Sale stage.""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "http://akretion.com",
+    "depends": [],
+    "data": ["security/crm_aftersale_security.xml"],
+    "demo": [],
+}
