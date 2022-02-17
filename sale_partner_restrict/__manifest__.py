8610f6ce7f5e52f56e4b9b6806eb67651b0107e2
clementmbr <clement.mombereau@akretion.com.br>
2022-02-17 14:09:06 -0300

    [IMP] sale_partner_restrict OK

--- sale_partner_restrict/__manifest__.py
+++ sale_partner_restrict/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Partner Restrict",
     "summary": """
-        Restrict security group "Sales/Own Documents Only" to CRUD on thei own res.partner records only.""",
+        Restrict security group "Internal Users" to CRUD on thei own res.partner records only.""",
     "description": """This module creates 2 new ir.rule defined like the rules used to restrict Salesmen on their own leads/orders.""",
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
